4837f38cc3fe118e407ec1927e5d0e5352a11834
Xaeina <67559845+xaiena@users.noreply.github.com>
2022-04-14 22:29:39 -0500

    icon

---
+++ app/src/main/java/com/example/logger/LogControl.java
@@ -0,0 +1,12 @@
+package com.example.logger;
+
+import com.sonyericsson.extras.liveware.aef.sensor.Sensor;
+import com.sonyericsson.extras.liveware.extension.util.control.ControlExtension;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensor;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensorEvent;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensorEventListener;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensorException;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensorManager;
+
+public class LogControl {
+}
---
+++ app/src/main/java/com/example/logger/LogRegistration.java
@@ -0,0 +1,56 @@
+package com.example.logger;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.sonyericsson.extras.liveware.aef.registration.Registration;
+import com.sonyericsson.extras.liveware.aef.sensor.Sensor;
+import com.sonyericsson.extras.liveware.extension.util.ExtensionUtils;
+import com.sonyericsson.extras.liveware.extension.util.sensor.AccessorySensor;
+
+public class LogRegistration extends com.sonyericsson.extras.liveware.extension.util.registration.RegistrationInformation {
+
+    private final Context mContext;
+
+    private static final String LOG_TAG = "SmartBandLogger";
+    private final String CLASS = getClass().getSimpleName();
+
+    protected LogRegistration(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("context == null");
+        }
+        mContext = context;
+    }
+
+    @Override
+    public int getRequiredControlApiVersion() { return 1; }
+
+    @Override
+    public int getRequiredSensorApiVersion() { return 1; }
+
+    @Override
+    public int getRequiredNotificationApiVersion() {
+        return com.sonyericsson.extras.liveware.extension.util.registration.RegistrationInformation.API_NOT_REQUIRED;
+    }
+
+    @Override
+    public int getRequiredWidgetApiVersion() {
+        return com.sonyericsson.extras.liveware.extension.util.registration.RegistrationInformation.API_NOT_REQUIRED;
+    }
+
+    /**
+     * Get the extension registration information.
+     *
+     * @return The registration configuration.
+     */
+
+    @Override
+    public ContentValues getExtensionRegistrationConfiguration() {
+        String iconHostapp = ExtensionUtils.getUriString(mContext, R.drawable.icon);
+        String iconExtension = ExtensionUtils.getUriString(mContext, R.drawable.icon);
+
+        ContentValues values = new ContentValues();
+
+        return null;
+    }
+}
